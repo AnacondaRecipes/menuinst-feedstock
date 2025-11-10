12b9b53f2ff71689296ce747586e36662b50fba9
Andrii Osipov <aosipov@anaconda.com>
2025-11-10 03:32:33 -0800

    fix one more time win

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -9,13 +9,13 @@ IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
 echo. > "%PREFIX%\.nonadmin"
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
 
-:: Run tests only if Python < 3.14
-if "%PYTHON_VERSION%"!="3.14"
-    :: Run the tests
+:: Run tests only if Python is NOT 3.14
+if not "%PYTHON_VERSION%"=="3.14" (
+    echo Running tests for Python %PYTHON_VERSION%
     :: Cannot run tests in test_schema.py because hypothesis-jsonschema is not on defaults
     :: Cannot run others because privilege elevation is not possible on the build platform
     pytest tests\ -vvv --ignore=tests\test_schema.py --ignore=tests\test_elevation.py -k "not test_create_and_remove_shortcut"
-    IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
+    if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 ) else (
     echo Skipping tests on Python 3.14+
 )
