99ed63b084c2382ba7d616605d6554bc35f3210b
Andrii Osipov <aosipov@anaconda.com>
2025-11-10 02:05:24 -0800

    skip tests for py314

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -9,8 +9,13 @@ IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
 echo. > "%PREFIX%\.nonadmin"
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
 
-:: Run the tests
-:: Cannot run tests in test_schema.py because hypothesis-jsonschema is not on defaults
-:: Cannot run others because privilege elevation is not possible on the build platform
-pytest tests\ -vvv --ignore=tests\test_schema.py --ignore=tests\test_elevation.py -k "not test_create_and_remove_shortcut"
-IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
+:: Run tests only if Python < 3.14
+IF %PY_VERSION% LSS 314 (
+    :: Run the tests
+    :: Cannot run tests in test_schema.py because hypothesis-jsonschema is not on defaults
+    :: Cannot run others because privilege elevation is not possible on the build platform
+    pytest tests\ -vvv --ignore=tests\test_schema.py --ignore=tests\test_elevation.py -k "not test_create_and_remove_shortcut"
+    IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
+) else (
+    echo Skipping tests on Python 3.14+
+)
